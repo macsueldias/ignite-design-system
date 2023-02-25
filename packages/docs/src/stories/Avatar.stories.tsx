8af3853bb725f962bef612a2bda2c8cc73a1dc35
Macsuel Dias <macsueldias@hotmail.com>
2023-02-25 16:48:21 -0300

    story avatar button

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/macsueldias.png',
+    alt: 'Macsuel Dias',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
---
+++ packages/react/src/components/Avatar/index.tsx
@@ -0,0 +1,17 @@
+import { User } from 'phosphor-react'
+import { ComponentProps, ElementType } from 'react'
+import { AvatarContainer, AvatarFallback, AvatarImage } from './styles'
+
+export interface AvatarProps extends ComponentProps<typeof AvatarImage> {
+  as?: ElementType
+}
+export function Avatar(props: AvatarProps) {
+  return (
+    <AvatarContainer>
+      <AvatarImage {...props} />
+      <AvatarFallback delayMs={600}>
+        <User />
+      </AvatarFallback>
+    </AvatarContainer>
+  )
+}
