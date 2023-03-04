11f76a492a89b90751d3fd40ec165e8baa6e84a6
Macsuel Dias <macsueldias@hotmail.com>
2023-03-04 16:23:30 -0300

    Add color test

--- packages/react/dist/index.mjs
+++ packages/react/dist/index.mjs
@@ -45,7 +45,8 @@ var colors = {
   ignite300: "#00B37E",
   ignite500: "#00875F",
   ignite700: "#015F43",
-  ignite900: "#00291D"
+  ignite900: "#00291D",
+  test: "#9f1134"
 };
 var space = {
   1: "0.25rem",
