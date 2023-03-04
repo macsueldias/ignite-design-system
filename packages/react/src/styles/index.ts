dfe920bbbaf608326d7b9fded5b702c3ad6b11b5
Macsuel Dias <macsueldias@hotmail.com>
2023-03-04 00:26:12 -0300

    fix update name

--- packages/react/src/styles/index.ts
+++ packages/react/src/styles/index.ts
@@ -6,7 +6,7 @@ import {
   lineHeights,
   radii,
   space,
-} from '@ignite-ui/tokens'
+} from '@interfacex-ui/tokens'
 import { createStitches, defaultThemeMap } from '@stitches/react'
 
 createStitches({
